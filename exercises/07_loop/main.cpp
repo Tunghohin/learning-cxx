a82f4713a6e2477fa12a2b59fdcb5e8bcce57a9f
Tunghohin <houhin0809@gmail.com>
2026-01-04 21:31:15 +0800

    finished

--- exercises/07_loop/main.cpp
+++ exercises/07_loop/main.cpp
@@ -6,9 +6,14 @@
 static unsigned long long fibonacci(int i) {
     // TODO: 为缓存设置正确的初始值
     static unsigned long long cache[96], cached;
+    cache[1] = cache[2] = 1;
+    cached = 0;
     // TODO: 设置正确的循环条件
-    for (; false; ++cached) {
-        cache[cached] = cache[cached - 1] + cache[cached - 2];
+    for (; cached < 96; ++cached) {
+        if (cache[cached] == 0) {
+            cache[cached] = cache[cached - 1] + cache[cached - 2];
+        }
+        if (cached == i) break;
     }
     return cache[i];
 }
